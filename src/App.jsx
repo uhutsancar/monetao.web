626a7800ce6410e5fd3612984cf97564a378ad9f
Uhut Sancar <sancaruhut@gmail.com>
2025-07-29 13:36:35 +0300

    login and signup page designs completed

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/expense" element={<Expense />} />
+          <Route path="/income" element={<Income />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+const Root = () => {
+  //check if token exists ins localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //redirect to dashboard if authenticated , otherwise to login
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
---
+++ src/pages/Auth/SignUp.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "~/components/Inputs/Input";
+import ProfilePhotoSelector from "~/components/Inputs/ProfilePhotoSelector";
+import AuthLayout from "~/components/layouts/AuthLayout";
+import { validateEmail } from "~/utils/helper";
+
+const SignUp = () => {
+  const [profilPic, setProfilPic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  //handle Sign Up from submit
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl = "";
+
+    if (!fullName) {
+      setError("Please enter your name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password.");
+      return;
+    }
+
+    setError("");
+
+    //Sign Up API Call
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilPic} setImage={setProfilPic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            />
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 Characters"
+                type="password"
+              />
+            </div>
+          </div>
+
+          {error && <p className="text-red-500  text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            Sign Up
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Already have an account?
+            <Link className="font-medium text-primary underline" to="/login">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
